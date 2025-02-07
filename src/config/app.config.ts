f137efef2883778f8093bbe36dde6be8d1245239
Surajit-FR <miltonbaker.psoriatic@gmail.com>
2025-02-07 19:36:49 +0530

    ssl

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
-// export const REACT_APP_BASE_URL: string = "https://theassure.co.uk/api/v1";
-export const REACT_APP_BASE_URL: string = "http://13.201.229.132:7000/api/v1";
+export const REACT_APP_BASE_URL: string = "https://theassure.co.uk/api/v1";
+// export const REACT_APP_BASE_URL: string = "http://13.201.229.132:7000/api/v1";
 // export const REACT_APP_BASE_URL: string = "http://localhost:7000/api/v1";
 // export const REACT_APP_BASE_URL: string = "http://192.168.9.111:7000/api/v1";
 export const REACT_APP_SECRET_KEY: string = "Eyul6X4uytwbDS&EA7Cv0Xj5C1*%j|BA5DwskcPnvex44q1WjsFr";
